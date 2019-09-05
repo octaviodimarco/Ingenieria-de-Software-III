a15c0dc9340cf2feaef07d96c20feb081122f719
Octavio <1610251@ucc.edu.ar>
2019-09-05 18:16:31 -0300

    Agregado RESTful

--- trabajo-practico-3/payroll/server/src/main/java/payroll/EmployeeController.java
+++ trabajo-practico-3/payroll/server/src/main/java/payroll/EmployeeController.java
@@ -1,7 +1,12 @@
 package payroll;
 
-import java.util.List;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +26,20 @@ class EmployeeController {
 
 	// Aggregate root
 
+	// tag::get-aggregate-root[]
 	@GetMapping("/employees")
-	List<Employee> all() {
-		return repository.findAll();
+	Resources<Resource<Employee>> all() {
+
+		List<Resource<Employee>> employees = repository.findAll().stream()
+			.map(employee -> new Resource<>(employee,
+				linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+				linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+			.collect(Collectors.toList());
+		
+		return new Resources<>(employees,
+			linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
 	}
+	// end::get-aggregate-root[]
 
 	@PostMapping("/employees")
 	Employee newEmployee(@RequestBody Employee newEmployee) {
@@ -32,13 +47,19 @@ class EmployeeController {
 	}
 
 	// Single item
-	
+
+	// tag::get-single-item[]
 	@GetMapping("/employees/{id}")
-	Employee one(@PathVariable Long id) {
+	Resource<Employee> one(@PathVariable Long id) {
 		
-		return repository.findById(id)
+		Employee employee = repository.findById(id)
 			.orElseThrow(() -> new EmployeeNotFoundException(id));
+		
+		return new Resource<>(employee,
+			linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+			linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
 	}
+	// end::get-single-item[]
 
 	@PutMapping("/employees/{id}")
 	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
