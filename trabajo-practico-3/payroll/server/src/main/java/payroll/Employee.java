f83e8b3e9bcfe43b3fd6d80f58c969e9f3a5082b
Octavio <1610251@ucc.edu.ar>
2019-09-07 14:28:25 -0300

    Evolution

--- trabajo-practico-3/payroll/server/src/main/java/payroll/Employee.java
+++ trabajo-practico-3/payroll/server/src/main/java/payroll/Employee.java
@@ -11,13 +11,25 @@ import javax.persistence.Id;
 class Employee {
 
 	private @Id @GeneratedValue Long id;
-	private String name;
+	private String firstName;
+	private String lastName;
 	private String role;
 
 	Employee() {}
 
-	Employee(String name, String role) {
-		this.name = name;
+	Employee(String firstName, String lastName, String role) {
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.role = role;
+	}
+
+	public String getName() {
+		return this.firstName + " " + this.lastName;
+	}
+
+	public void setName(String name) {
+		String[] parts =name.split(" ");
+		this.firstName = parts[0];
+		this.lastName = parts[1];
 	}
 }
